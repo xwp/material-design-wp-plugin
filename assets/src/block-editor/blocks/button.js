58e0c576c31952477c3b53a29d226f05fdb4acd9
Dukagjin Surdulli <me@dukagj.in>
2020-02-13 15:59:59 +0100

    Replace Material React Components with Web Components by Google

--- assets/src/block-editor/blocks/button.js
+++ assets/src/block-editor/blocks/button.js
@@ -1,20 +1,25 @@
-import { Icon } from '@rmwc/icon';
-import { Button as MdcButton } from '@rmwc/button';
-
-import '@material/button/dist/mdc.button.css';
+import '@material/mwc-button';
 
 export const button = {
 	name: 'button',
 	block: {
 		title: 'MDC Button',
-		icon: <Icon icon="favorite" />,
+		icon: <i className="material-icons md-light">face</i>,
 		category: 'layout',
 		example: {},
 		edit() {
-			return <MdcButton>Hello World</MdcButton>;
+			return (
+				<mwc-button id="myButton" raised icon="code">
+					Hello World
+				</mwc-button>
+			);
 		},
 		save() {
-			return <MdcButton>Hello World</MdcButton>;
+			return (
+				<mwc-button id="myButton" raised icon="code">
+					Hello World
+				</mwc-button>
+			);
 		},
 	},
 };
--- assets/src/block-editor/index.js
+++ assets/src/block-editor/index.js
@@ -1,18 +1,11 @@
-// Add the JS code to this file. On running npm run dev, it will compile to assets/js/.
-
-/**
- * Internal dependencies
- */
-import 'material-design-icons/iconfont/material-icons.css';
-import '@rmwc/icon/icon.css';
-import './edit.css';
-
 import { registerBlockType } from '@wordpress/blocks';
 import { button } from './blocks/button';
-import { tabBar } from './blocks/tab-bar';
+
+import 'material-design-icons/iconfont/material-icons.css';
+import './edit.css';
 
 registerCustomBlock( button );
-registerCustomBlock( tabBar );
+// registerCustomBlock( tabBar );
 
 function registerCustomBlock( { name, block } ) {
 	registerBlockType( 'material-theme-builder/' + name, block );
